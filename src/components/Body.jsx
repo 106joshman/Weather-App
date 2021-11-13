ad37ecaa935b1b490c70f672989eb9a3b2dea89e
Joshman <ejembijoshman@gmail.com>
2021-11-13 18:15:34 +0100

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,9 @@
+const Body = () => {
+    return (
+        <div className="wrapper">
+            <h1>Weather App</h1>
+        </div>
+    );
+}
+ 
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="head-logo">
+                <span>D</span>ev- <span>G</span>uy
+            </div>
+            <div className="link">
+                <a href="mailto:ejembijoshman@gmail.com">Contact</a>
+            </div>
+        </div>
+    );
+}
+ 
+export default Header;
