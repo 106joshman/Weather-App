871b2286208294a2f08518868d48d60455dc1c6d
Joshman <ejembijoshman@gmail.com>
2021-11-14 02:24:11 +0100

    updates and changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,21 @@
+import { useState, useEffect } from 'react';
+
 const Body = () => {
+    // const [info, setInfo] = useState({});
+    
     return (
         <div className="wrapper">
-            <h1>Weather App</h1>
+            <div className="title">
+                <h5>Weather App</h5>
+            </div>
+            <div className="wrapem">
+                <div className="weather present"></div>
+                <div className="weather future">
+                    <div className="cards">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,12 @@ const Header = () => {
             <div className="head-logo">
                 <span>D</span>ev- <span>G</span>uy
             </div>
+            <div className="search-box">
+                <form action="to get location" method="get">
+                    <input type="search" className="Search" name="search-box" placeholder="Enter city or location here" id=""/>
+                    <button type="submit">Search</button>
+                </form>
+            </div>
             <div className="link">
                 <a href="mailto:ejembijoshman@gmail.com">Contact</a>
             </div>
